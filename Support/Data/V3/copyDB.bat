015a82596bec4bf22a2ee5135952ad32ddcb7ca5
Derek Wilson <mail@derekwilson.net>
2026-05-14 21:17:29 +1200

    copy DB to MAUI app

--- Support/Data/V3/copyDB.bat
+++ Support/Data/V3/copyDB.bat
@@ -1,4 +1,5 @@
 copy worldolio.sqlite ..\..\..\Worldolio.NET\POC\WorldolioDataChecker\bin\Debug\net8.0
 copy worldolio.sqlite ..\..\..\Worldolio.NET\POC\WorldolioPOC\bin\Debug\net8.0
 copy worldolio.sqlite ..\..\..\Worldolio.NET\POC\WorldolioMauiPOC\bin\Debug\net8.0-windows10.0.19041.0\win10-x64\AppX
+copy worldolio.sqlite ..\..\..\Worldolio.NET\POC\WorldolioMauiPOC\Resources\Raw\worldolio.sqlite
 copy worldolio.sqlite ..\..\..\Worldolio.NET\Applications\WorldolioCLI\bin\Debug\net8.0
