1ee5e938125f9f546e49e3a654d061f9be32f662
Derek Wilson <mail@derekwilson.net>
2026-09-01 20:34:10 +1200

    added ability to edit the city ids and persist them in settings

--- Worldolio.NET/POC/WorldolioMauiPOC/AppSettings/UserSettings.cs
+++ Worldolio.NET/POC/WorldolioMauiPOC/AppSettings/UserSettings.cs
@@ -1,12 +1,47 @@
-﻿namespace WorldolioMauiPOC.AppSettings
+﻿using Worldolio.Data.Logging;
+
+namespace WorldolioMauiPOC.AppSettings
 {
     public interface IUserSettings
     {
+        long[] DefaultCities { get; set; }
+
         long[] Cities { get; set; }
+
+        void SetFromString(String ids, bool store);
     }
+
     public class UserSettings : IUserSettings
     {
-        private long[] _cities = [458, 252, 477, 324, 79, 320, 279, 429, 180, 351];
+        private ILogger _logger;
+
+        private const string CITY_IDS_KEY = "city_ids";
+
+        public UserSettings(ILogger logger)
+        {
+            _logger = logger;
+
+            logger.Debug(() => $"UserSettings init:");
+            string cityIdsFromPrefs = Preferences.Default.Get(CITY_IDS_KEY, String.Join(',', _defaultcities));
+            SetFromString(cityIdsFromPrefs, false);
+            logger.Debug(() => $"UserSettings init: [{String.Join(',',_cities)}]");
+        }
+
+        //private long[] _defaultcities = [458, 252, 477];
+        private long[] _defaultcities = [458, 252, 477, 324, 79, 320, 279];
+        private long[] _cities = [];
+
+        public long[] DefaultCities
+        {
+            get
+            {
+                return _defaultcities;
+            }
+            set
+            {
+                _defaultcities = value;
+            }
+        }
 
         public long[] Cities
         {
@@ -17,6 +52,35 @@
             set
             {
                 _cities = value;
+            }
+        }
+
+        public void SetFromString(string ids, bool store)
+        {
+            _logger.Debug(() => $"UserSettings SetFromString: {ids}");
+            var idArray = ids.Split(',');
+            if (idArray.Length < 1)
+            {
+                _logger.Debug(() => $"UserSettings SetFromString: invalid {ids}");
+                return;
+            }
+            List<long> idsAsLong = new List<long>();
+            foreach (var id in idArray)
+            {
+                if (long.TryParse(id, out long result))
+                {
+                    _logger.Debug(() => $"UserSettings SetFromString: adding {result}");
+                    idsAsLong.Add(result);
+                }
+                else
+                {
+                    _logger.Debug(() => $"UserSettings SetFromString: cannot parse {id}");
+                }
+            }
+            _cities = idsAsLong.ToArray();
+            if (store)
+            {
+                Preferences.Default.Set(CITY_IDS_KEY, String.Join(',', _cities));
             }
         }
     }
--- Worldolio.NET/POC/WorldolioMauiPOC/ViewModels/Settings/SettingsViewModel.cs
+++ Worldolio.NET/POC/WorldolioMauiPOC/ViewModels/Settings/SettingsViewModel.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Windows.Input;
 using Worldolio.Data.Logging;
+using WorldolioMauiPOC.AppSettings;
 using WorldolioMauiPOC.Utility;
 
 namespace WorldolioMauiPOC.ViewModels.Settings
@@ -9,22 +10,43 @@ namespace WorldolioMauiPOC.ViewModels.Settings
     public partial class SettingsViewModel : INotifyPropertyChanged
     {
         public ICommand NavigateBack { get; }
+        public ICommand ResetIds { get; }
+        public ICommand UpdateIds { get; }
+
+        public string CurrentSettingsCityIds { get; set; } = "";
 
         private ILogger _logger;
         private INavigationHelper _navigationHelper;
+        private IUserSettings _userSettings;
 
         public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged(string name) =>
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
-        public SettingsViewModel(ILogger logger, INavigationHelper navigationHelper)
+        public SettingsViewModel(ILogger logger, INavigationHelper navigationHelper, IUserSettings userSettings)
         {
             logger.Debug(() => $"SettingsViewModel init");
 
             _logger = logger;
             _navigationHelper = navigationHelper;
+            _userSettings = userSettings;
+
+            CurrentSettingsCityIds = String.Join(',', _userSettings.Cities);
 
             NavigateBack = new Command(async () => await _navigationHelper.ExecuteNavigationAsync(".."));
+            ResetIds = new Command(() =>
+            {
+                _logger.Debug(() => $"ResetIds");
+                CurrentSettingsCityIds = String.Join(',', _userSettings.DefaultCities);
+                OnPropertyChanged("CurrentSettingsCityIds");
+            });
+            UpdateIds = new Command(() =>
+            {
+                _logger.Debug(() => $"UpdateIds {CurrentSettingsCityIds}");
+                _userSettings.SetFromString(CurrentSettingsCityIds, true);
+                CurrentSettingsCityIds = String.Join(',', _userSettings.Cities);
+                OnPropertyChanged("CurrentSettingsCityIds");
+            });
         }
 
         [RelayCommand]
