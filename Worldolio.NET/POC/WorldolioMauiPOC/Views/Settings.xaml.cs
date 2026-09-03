c3edd20dccba9a1a08ba3e1fb7d60f060688d3e0
Derek Wilson <mail@derekwilson.net>
2026-09-03 12:09:20 +1200

    fixed soft keyboard not going away when the settings are closed

--- Worldolio.NET/POC/WorldolioMauiPOC/Views/Settings.xaml.cs
+++ Worldolio.NET/POC/WorldolioMauiPOC/Views/Settings.xaml.cs
@@ -1,11 +1,13 @@
 using Worldolio.Data.Logging;
 using WorldolioMauiPOC.ViewModels.Settings;
+using CommunityToolkit.Maui.Core.Platform;
 
 namespace WorldolioMauiPOC.Views;
 
 public partial class Settings : ContentPage
 {
     private ILogger _logger;
+    private SettingsViewModel _viewModel;
     
 	public Settings(SettingsViewModel viewModel, ILogger logger)
 	{
@@ -13,7 +15,15 @@ public partial class Settings : ContentPage
         InitializeComponent();
 
         _logger = logger;
+        _viewModel = viewModel;
 
         BindingContext = viewModel;
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        // on android the keyboard does not go away when the screen is closed
+        SettingIdsEntry.HideKeyboardAsync(CancellationToken.None);
     }
 }
