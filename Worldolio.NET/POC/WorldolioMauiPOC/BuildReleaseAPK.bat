742a513a3c0c7d5261b406cfccfd2bbed1867c27
Derek Wilson <mail@derekwilson.net>
2026-09-10 11:57:12 +1200

    Updated APK build script for .Net 10 update

--- Worldolio.NET/POC/WorldolioMauiPOC/BuildReleaseAPK.bat
+++ Worldolio.NET/POC/WorldolioMauiPOC/BuildReleaseAPK.bat
@@ -1,5 +1,5 @@
-del bin\Release\net8.0-android\net.derekwilson.worldoliomauipoc*.apk
+del bin\Release\net10.0-android\net.derekwilson.worldoliomauipoc*.apk
 dotnet clean --configuration Release
-dotnet publish -c Release -f net8.0-android -p:AndroidPackageFormats=apk
-copy bin\Release\net8.0-android\net.derekwilson.worldoliomauipoc-Signed.apk ..\..\..\Support\CurrentBuild\Android
+dotnet publish -c Release -f net10.0-android -p:AndroidSdkDirectory="D:\AndroidSDK" -p:AndroidPackageFormats=apk
+copy bin\Release\net10.0-android\net.derekwilson.worldoliomauipoc-Signed.apk ..\..\..\Support\CurrentBuild\Android
 pause
